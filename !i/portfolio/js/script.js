f1ca7df9ab44ebb0e1e75caac6feafa998a1e3ac
ted <eeeeeddddd@hotmail.com>
2018-01-06 14:52:57 +0000

    transform fix rotate

--- !i/portfolio/js/script.js
+++ !i/portfolio/js/script.js
@@ -82,36 +82,6 @@ $(document).ready (function() {
 		$("textarea, input").css ({background: "#f5f5f5"});	})
 ;});
 
-//------------------------------
-
-$(function(){
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 300) {
-            $('.line1').addClass('percent85');
-            $('.line2').addClass('percent90');
-            $('.line3').addClass('percent85');
-            $('.line4').addClass('percent70');
-            $('.line5').addClass('percent70');
-            $('.line6').addClass('percent85');
-            $('.line7').addClass('percent85');
-            $('.line8').addClass('percent70');
-            $('.line9').addClass('percent70');
-            $('.line10').addClass('percent70');            
-        }
-        else {
-            $('.line1').removeClass('percent85');
-            $('.line2').removeClass('percent90');
-            $('.line3').removeClass('percent85');
-            $('.line4').removeClass('percent70');
-            $('.line5').removeClass('percent70');
-            $('.line6').removeClass('percent85');
-            $('.line7').removeClass('percent85');
-            $('.line8').removeClass('percent70');
-            $('.line9').removeClass('percent70');
-            $('.line10').removeClass('percent70');            
-        }
-    });
-});
 
 // //smooth scroll
 $(document).ready(function(){
